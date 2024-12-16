16d4847a6b838aed44440113d5bb92674d722ea6
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-12-16 11:36:16 +0000

    Stop a user that is not signed in from accessing secret code routes

--- routes/secretCodeRouter.mjs
+++ routes/secretCodeRouter.mjs
@@ -1,13 +1,14 @@
 import * as secretCodeController from '../controllers/secretCodeController.mjs';
+import { isAuth } from './auth.mjs';
 import { Router } from 'express';
 
 const appRouter = Router();
 
-appRouter.get('/', secretCodeController.getSecretCodeSelect);
-appRouter.get('/member', secretCodeController.getMemberCodeForm);
-appRouter.get('/admin', secretCodeController.getAdminCodeForm);
-appRouter.get('/success', secretCodeController.getSuccess);
-appRouter.post('/member', secretCodeController.postMemberCodeForm);
-appRouter.post('/admin', secretCodeController.postAdminCodeForm);
+appRouter.get('/', isAuth, secretCodeController.getSecretCodeSelect);
+appRouter.get('/member', isAuth, secretCodeController.getMemberCodeForm);
+appRouter.get('/admin', isAuth, secretCodeController.getAdminCodeForm);
+appRouter.get('/success', isAuth, secretCodeController.getSuccess);
+appRouter.post('/member', isAuth, secretCodeController.postMemberCodeForm);
+appRouter.post('/admin', isAuth, secretCodeController.postAdminCodeForm);
 
 export default appRouter;
