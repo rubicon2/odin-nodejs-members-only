5da96c8dd1e39daea43cc86dd7e50b5f1496c9f1
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-01-07 12:03:46 +0000

    Fix: spelling errors in comments

--- middleware/storeValidationErrors.mjs
+++ middleware/storeValidationErrors.mjs
@@ -2,7 +2,7 @@ import { validationResult } from 'express-validator';
 
 function storeValidationErrors(req, res, next) {
   const result = validationResult(req);
-  // Structure data so we can put errors next to relevant form ields.
+  // Structure data so we can put errors next to relevant form fields.
   req.session.errors = result.array().reduce(
     (obj, current) => {
       return {
@@ -10,7 +10,7 @@ function storeValidationErrors(req, res, next) {
         [current.path]: current.msg,
       };
     },
-    // And hold an array of the same messages to ejs can easily loop through.
+    // And hold an array of the same messages so ejs can easily loop through.
     { array: result.array().map((error) => error.msg) },
   );
   next();
