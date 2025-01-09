28ff4075d05ccfcdeaa7c9068e770a499388680a
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-01-09 11:38:04 +0000

    Encode passport messages as JSON so view knows which error is for which field
    Passport only allows messages to be strings. Had a
    hacky workaround so that the email form field could
    have an error if the email/user does not exist, and
    the password field an error if the user/password
    does not match - but this is a better solution.
    
    I think I just totally forgot JSON existed...

--- middleware/storePassportErrors.mjs
+++ middleware/storePassportErrors.mjs
@@ -1,24 +1,19 @@
 function storePassportErrors(req, res, next) {
   // Turn passport errors into a data structure consistent with form validation errors.
   // This way, we can re-use the errors view instead of having a separate one for passport messages.
-  // But it is rather annoying that we can't store what field the error relates to.
-  // Trying to pass anything other than a string as a message just doesn't work.
-  const array = req.session.messages ? [...req.session.messages] : [];
-  const obj = array.reduce((obj, msg) => {
-    // Hacky but don't really see any other way to tell the view
-    // which field is incorrect. Passport only seems to let us set a string message.
-    if (msg === 'The email and password do not match') {
-      return {
-        ...obj,
-        password: msg,
-      };
-    } else {
-      return {
-        ...obj,
-        email: msg,
-      };
-    }
+
+  // In case there are no messages, this will stop array map throwing an error.
+  if (!req.session.messages) req.session.messages = [];
+
+  // Each message is a JSON string due to passport only allowing string messages.
+  const array = req.session.messages.map((message) => JSON.parse(message));
+  const obj = array.reduce((obj, current) => {
+    return {
+      ...obj,
+      [current.path]: current.msg,
+    };
   }, {});
+
   req.session.errors = { ...obj, array };
   delete req.session.messages;
   req.session.save();
