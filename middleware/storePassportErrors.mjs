b5fc40b0fc80e7547a685fc3b4b69b1763961979
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-01-09 11:22:15 +0000

    Show error on a specific form field when logging in
    Passport message does not allow objects, only strings.
    This is a hack solution. Could try storing messages
    in the form of JSON objects.

--- middleware/storePassportErrors.mjs
+++ middleware/storePassportErrors.mjs
@@ -4,7 +4,22 @@ function storePassportErrors(req, res, next) {
   // But it is rather annoying that we can't store what field the error relates to.
   // Trying to pass anything other than a string as a message just doesn't work.
   const array = req.session.messages ? [...req.session.messages] : [];
-  req.session.errors = { array };
+  const obj = array.reduce((obj, msg) => {
+    // Hacky but don't really see any other way to tell the view
+    // which field is incorrect. Passport only seems to let us set a string message.
+    if (msg === 'The email and password do not match') {
+      return {
+        ...obj,
+        password: msg,
+      };
+    } else {
+      return {
+        ...obj,
+        email: msg,
+      };
+    }
+  }, {});
+  req.session.errors = { ...obj, array };
   delete req.session.messages;
   req.session.save();
   next();
