d70f97d057293896674139645c470d101f2f6642
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-01-10 12:01:42 +0000

    Add connect-pg-simple setup sql script into db populate script

--- db/populate.js
+++ db/populate.js
@@ -3,6 +3,18 @@
 const { Client } = require('pg');
 
 const SQL = `
+  CREATE TABLE IF NOT EXISTS "session" (
+    "sid" varchar NOT NULL COLLATE "default",
+    "sess" json NOT NULL,
+    "expire" timestamp(6) NOT NULL
+  )
+  WITH (OIDS=FALSE);
+
+  ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+  CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
+
+
   CREATE TABLE IF NOT EXISTS app_password (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     type VARCHAR (255) NOT NULL,
